a507c9673d0e88567b2ad3eb3e78dbbe0026d72e
Excalibur0331 <eli1838459978@gmail.com>
2024-12-19 01:39:13 -0800

    add fn ui and bn

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
-import { useState } from 'react'
 import { APIProvider } from '@vis.gl/react-google-maps';
-import './App.css'
-import AddressAutocomplete from './compoents/AddressAutocomplete'
+import './App.css';
+import AddressAutocomplete from './components/AddressAutocomplete';
 
 function App() {
-  console.log('Google Maps API Key:', import.meta.env.VITE_GOOGLE_MAPS_API_KEY);
-
   return (
-    <>
-      <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
-        <div>
+    <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
+      <div className="app-container">
+        <header className="app-header">
           <h1>Property Search</h1>
-          <AddressAutocomplete />
-        </div>
-      </APIProvider>
-    </>
-  )
+          <p>Enter an address to get detailed property information and nearby schools</p>
+        </header>
+        <AddressAutocomplete />
+      </div>
+    </APIProvider>
+  );
 }
 
-export default App
+export default App;
